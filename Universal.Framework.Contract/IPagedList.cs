963b19fc34e022deeca5b80283e689d33f52c15c
wslpeter <wslpeter@163.com>
2015-10-05 11:18:42 +0800

    初始化Framework 业务无关的底层通用机制及功能

---
+++ Universal.Framework.Contract/IPagedList.cs
@@ -0,0 +1,9 @@
+namespace Universal.Framework.Contract
+{
+    public interface IPagedList
+    {
+        int CurrentPageIndex { get; set; }
+        int PageSize { get; set; }
+        int TotalItemCount { get; set; }
+    }
+}
